c670abb06520fec908f6827c4e9bd19848f6c376
gimhan <gimhansandeep96@gmail.com>
2026-09-15 20:14:45 +0530

    Init Springboot

---
+++ src/main/java/lk/ijse/online/movie/booking/system/OnlineMovieBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.online.movie.booking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMovieBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMovieBookingSystemApplication.class, args);
+	}
+
+}
